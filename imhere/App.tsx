04de79dbffc5be048f3139ca0b2c7ed398542b70
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-02 11:52:46 -0300

    feat: Diference between import default and import without defult.

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,6 +1,8 @@
 import Home from './src/screens/home';
 
 
+// Export Default --> who will import dont need to add {} to specified with module will be imported.
+// Export with Default --> Who will import need to specify with module is imported in your file.
 export default function App() {
   return (
     <Home />
