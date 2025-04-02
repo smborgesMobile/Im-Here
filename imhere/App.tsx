a7102b77013b33f56f3f67591049e57a7329959f
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-02 11:23:12 -0300

    feat: Add style date

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,12 +1,13 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.eventName}>
-        Sérgio Mendes!
-      </Text>
+      <View>
+        <Text style={styles.eventName}> Sérgio Mendes! </Text>
+        <Text style={styles.eventDate}> 12/02/1998! </Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -19,9 +20,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   eventName: {
-    flex: 1,
     fontSize: 32,
     fontWeight: 'bold',
     color: '#4363d8',
+  },
+  eventDate: {
+    color: '#4363d8',
+    fontSize: 20,
+    marginTop: 10,
   }
 });
